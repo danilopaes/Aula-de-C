38e632590474233b26a20982dd642a3b69b97cb8
danilopaes <danilopaesribeiro@gmail.com>
2022-07-01 19:59:09 -0300

    Add files via upload

---
+++ lib_pilha_lista/lib_pilha_lista.h
@@ -0,0 +1,16 @@
+#ifndef LIB_PILHA_LISTA_H_INCLUDED
+#define LIB_PILHA_LISTA_H_INCLUDED
+
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct pilha PILHA;
+typedef struct no NO;
+
+PILHA * pilha_cria();
+void pilha_push(PILHA *p, float valor);
+float pilha_pop(PILHA *p);
+int pilha_vazia(PILHA *p);
+void pilha_free(PILHA *p);
+
+#endif // LIB_PILHA_LISTA_H_INCLUDED
